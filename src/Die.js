716b3832dfe3503dfdfa49f56538e426b6b46866
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2024-11-04 16:26:05 +0530

    luckyN solved

--- src/Die.js
+++ src/Die.js
@@ -3,7 +3,6 @@ import "./Die.css";
 export default function Die({val, color}){
     return(
         <div className="Die" style={{ backgroundColor : color}}>
-            <h1> die component start </h1>
             {val}
         </div>
     );
--- src/LuckyN.js
+++ src/LuckyN.js
@@ -2,11 +2,11 @@ import { useState } from "react";
 import { getRolls } from "./utils";
 import Dice from "./Dice";
 
-export default function LuckyN( numDice = 2, goal = 7){
+export default function LuckyN( {numDice = 2, goal = 7} ){
     const [dice, setDice] = useState(getRolls(numDice));
     return(
         <main className = "LuckyN">
-            <h1> LuckyN </h1>
+            <h1> Lucky{goal} </h1>
             <Dice dice={dice} />
         </main>
     );

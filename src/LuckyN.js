356f17afac6b8bfa4e1c15134f931a493a0069f6
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2024-11-04 17:17:54 +0530

    re roll

--- src/LuckyN.js
+++ src/LuckyN.js
@@ -1,13 +1,17 @@
 import { useState } from "react";
-import { getRolls } from "./utils";
+import { getRolls, sum } from "./utils";
 import Dice from "./Dice";
 
 export default function LuckyN( {numDice = 2, goal = 7} ){
     const [dice, setDice] = useState(getRolls(numDice));
+    const isWinner = sum(dice) === goal;
+    const reRoll = ()=> setDice(getRolls(numDice));
     return(
         <main className = "LuckyN">
             <h1> Lucky{goal} </h1>
+            <h2> {isWinner && "You won the GAME"}</h2>
             <Dice dice={dice} />
+            <button onClick={reRoll}> Re-Roll </button>
         </main>
     );
 }
